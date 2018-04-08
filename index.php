3b56d1f5349eaf761bfdceb1d108e26ec69f49e6
Karan Kanojia <37948925+darkRook@users.noreply.github.com>
2018-04-08 14:32:07 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>darkRook</title>
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+	
+	<link href="https://fonts.googleapis.com/css?family=IBM+Plex+Mono" rel="stylesheet"> 
+	
+	 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+	 <link rel = "stylesheet" type = "text/css" href = "css/index.css" />
+	 
+	 
+
+</head>
+<body>
+	<div class="loder-app-class" id="loader-app-id"></div>
+	<div class="main-app-class" id="main-app-id">
+		
+	</div>
+
+	<script type="text/javascript" src="js/editorial.js"></script>
+	<script type="text/javascript" src="js/gallery.js"></script>
+
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
+	<script defer src="https://use.fontawesome.com/releases/v5.0.9/js/all.js" integrity="sha384-8iPTk2s/jMVj81dnzb/iFR2sdA7u06vHJyyLlAd4snFpCl/SnyUjRrbdJsw1pGIl" crossorigin="anonymous"></script>
+
+
+
+</body>
+</html>
